0ba77aa48479180ce3b357b083b8050e5f8d3674
Hellen Ferrari <hellenc.ferrari@gmail.com>
2023-03-31 13:07:21 -0700

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
