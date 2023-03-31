a15755642fa10b7c405cf2641f8e9fd804b1e980
Hellen Ferrari <hellenc.ferrari@gmail.com>
2023-03-31 13:19:15 -0700

    Create PostsList UI file

---
+++ SocialMedia/UI/PostsList.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostsList_Previews: PreviewProvider {
+    static var previews: some View {
+        PostsList()
+    }
+}
