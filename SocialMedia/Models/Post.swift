9b07b0cc9a837242cc0ad9d9d74fb6d803c34191
Hellen Ferrari <hellenc.ferrari@gmail.com>
2023-04-03 16:41:55 -0700

    Add Post and User models

---
+++ SocialMedia/Models/Post.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import Foundation
+
+struct Post: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+
---
+++ SocialMedia/Models/User.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let name: String
+}
