5a38c910a3fe200b90f9d113eac931ad94e1fc4f
Hellen Ferrari <hellenc.ferrari@gmail.com>
2023-04-03 16:43:42 -0700

    List all the Posts UI

--- SocialMedia/UI/PostsList.swift
+++ SocialMedia/UI/PostsList.swift
@@ -5,8 +5,42 @@
 import SwiftUI
 
 struct PostsList: View {
+    let service = Service()
+    @State var posts = [Post]()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List(posts, id: \.id) { post in
+                Section {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "")
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                                .backgroundStyle(.gray)
+                            
+                            Text("user")
+                        }
+
+                        Text(post.title)
+                            .font(Font.subheadline).bold()
+                            .padding(.top, 1)
+                        Text(post.body)
+                            .font(Font.body)
+                            .padding(.top, 1)
+                    }
+                }
+                
+            }
+            .navigationBarTitle("Posts", displayMode: .inline)
+        }
+        .onAppear {
+            Task {
+                posts = try await service.fetchPosts()
+            }
+            
+        }
     }
 }
 
