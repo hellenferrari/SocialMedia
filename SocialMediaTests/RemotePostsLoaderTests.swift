aa98ceba6a9090a7a46711b38f506953fc4a138e
Bogdan Poplauschi <bpoplauschi@gmail.com>
2023-04-21 11:06:25 +0300

    Updated tests per changes from urlsession to httpclient

--- SocialMediaTests/RemotePostsLoaderTests.swift
+++ SocialMediaTests/RemotePostsLoaderTests.swift
@@ -7,17 +7,37 @@ import XCTest
 
 final class RemotePostsLoaderTests: XCTestCase {
     func test_init() {
-        let session = MockURLSession()
-        let sut = RemotePostsLoader(session: session)
+        let sut = RemotePostsLoader(httpClient: MockHTTPClient())
         XCTAssertEqual(sut.endpoints.baseURL, "https://jsonplaceholder.typicode.com")
     }
     
-    func test_fetchPosts_onError() {
-        let session = MockURLSession()
-        let sut = RemotePostsLoader(session: session)
+    func test_fetchPosts_onError() async throws {
+        let httpClient = MockHTTPClient()
+        let anyError = anyNSError()
+        httpClient.mockedGetResult = .failure(anyError)
+        let sut = RemotePostsLoader(httpClient: httpClient)
         
-        // TODO
+        do {
+            _ = try await sut.fetchPosts()
+            XCTFail("Should throw and not get here")
+        } catch {
+            XCTAssertEqual(error as NSError, anyError)
+        }
     }
 }
 
-final class MockURLSession: URLSession {}
+final class MockHTTPClient: HTTPClient {
+    var mockedGetResult: Result<Data, Error> = .success(Data())
+    
+    func get(from url: URL) async -> Result<Data, Error> {
+        return mockedGetResult
+    }
+}
+
+func anyNSError() -> NSError { NSError(domain: "any domain", code: 0) }
+
+extension Post: Equatable {
+    public static func == (lhs: Post, rhs: Post) -> Bool {
+        lhs.id == rhs.id && lhs.userId == rhs.userId && lhs.title == rhs.title && lhs.body == rhs.body
+    }
+}
