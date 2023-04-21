273ddde082202d15a599a2ad18869563160d5db8
Bogdan Poplauschi <bpoplauschi@gmail.com>
2023-04-21 10:41:30 +0300

    Added basic URLSession implementation of the HTTPClient

--- SocialMedia/Network/URLSessionHTTPClient.swift
+++ SocialMedia/Network/URLSessionHTTPClient.swift
@@ -5,4 +5,12 @@
 import Foundation
 
 struct URLSessionHTTPClient: HTTPClient {
+    func get(from url: URL) async -> Result<Data, Error> {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return .success(data)
+        } catch {
+            return .failure(error)
+        }
+    }
 }
