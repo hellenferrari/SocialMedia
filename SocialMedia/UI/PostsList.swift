1e182889b5fc2de61ac9286ddb2635dcb92396ff
Bogdan Poplauschi <bpoplauschi@gmail.com>
2023-04-21 10:52:10 +0300

    Use Constructor Injection to pass a PostsViewModel dependency to the PostsList instead of it creating its own dependencies (per DIP). Used private helpers to improve readability of the view hierarachy. Added previews for the most basic states (empty, regular list, long list)

--- SocialMedia/UI/PostsList.swift
+++ SocialMedia/UI/PostsList.swift
@@ -5,34 +5,23 @@
 import SwiftUI
 
 struct PostsList: View {
-    @ObservedObject var viewModel = PostsViewModel()
+    @ObservedObject private var viewModel: PostsViewModel
+    
+    init(viewModel: PostsViewModel) {
+        self.viewModel = viewModel
+    }
     
     var body: some View {
         VStack {
             NavigationView {
-                List(viewModel.postsWithUser, id: \.post.id) { postDetail in
-                    Section {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Circle()
-                                    .fill(.gray)
-                                    .frame(width: 50, height: 50)
-                                
-                                Text(postDetail.user.name)
-                            }
-
-                            Text(postDetail.post.title)
-                                .font(Font.subheadline).bold()
-                                .padding(.top, 1)
-                            Text(postDetail.post.body)
-                                .font(Font.body)
-                                .padding(.top, 1)
-                        }
+                if viewModel.postsWithUser.isEmpty {
+                    VStack {
+                        Text("No posts available")
+                        Button("Reload", action: {})
                     }
-                    
+                } else {
+                    listView()
                 }
-                .navigationBarTitle("Posts", displayMode: .inline)
-                .listStyle(InsetListStyle())
             }
         }
         .onAppear {
@@ -41,10 +30,54 @@ struct PostsList: View {
             }
         }
     }
+    
+    private func listView() -> some View {
+        List(viewModel.postsWithUser, id: \.post.id) { postDetail in
+            Section {
+                postView(postDetail: postDetail)
+            }
+            
+        }
+        .navigationBarTitle("Posts", displayMode: .inline)
+        .listStyle(InsetListStyle())
+    }
+    
+    private func postView(postDetail: PostWithUser) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Circle()
+                    .fill(.gray)
+                    .frame(width: 50, height: 50)
+                
+                Text(postDetail.user.name)
+            }
+            
+            Text(postDetail.post.title)
+                .font(Font.subheadline).bold()
+                .padding(.top, 1)
+            Text(postDetail.post.body)
+                .font(Font.body)
+                .padding(.top, 1)
+        }
+    }
 }
+
+#if DEBUG
 
 struct PostsList_Previews: PreviewProvider {
     static var previews: some View {
-        PostsList()
+        Group {
+            PostsList(viewModel: PostsViewModel(service: DummyPostsLoader()))
+                .previewDisplayName("List")
+                        
+            PostsList(viewModel: PostsViewModel(service: DummyPostsLoader(posts: [])))
+                .previewDisplayName("Empty List")
+            
+            PostsList(viewModel: PostsViewModel(service: DummyPostsLoader(
+                posts: DummyPostsLoader.longListOfPosts)))
+                .previewDisplayName("Long List")
+        }
     }
 }
+
+#endif
